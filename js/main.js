6d60a6f433fd399548910b1d8b36006bbe0b6c31
Iulia2191 <ibocaniciu@gmail.com>
2023-06-08 10:02:35 +0200

    edit main.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,33 @@
 const subscribeButton = document.querySelector('.btn-newsletter')
 const subscribeInput = document.querySelector('#newsletterEmail')
 const errorContainer = document.querySelector('.subscribe-error-message')
-const newsletterContainer = document.querySelector('.newsletter')
-// Email Validation
+const newsletterContainer = document.querySelector('.newsletter-container')
+const submitContainer = document.querySelector('.submit-container')
 
-console.log('hi')
+
+function ValidateEmail() {
+  var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (subscribeInput.value.match(validRegex)) {
+    newsletterContainer.style.opacity = '0'
+    submitContainer.innerHTML =
+      '<p>Multumim pentru inregistrare. </br> Ne vom auzi in curand! 👏</p>'
+    subscribeForm.text1.focus();
+    return true;
+  } else {
+    errorContainer.innerHTML =
+      '<p>Email-ul nu este valid</p>'
+    return false;
+  }
+}
 
 subscribeButton.addEventListener('click', function (event) {
   event.preventDefault()
-  const emailValue = subscribeInput.value
-  const emailIsValid = emailValue.includes('@')
-  if (emailIsValid) {
-    newsletterContainer.style.opacity = '0'
-    errorContainer.innerHTML =
-      '<p>Multumim pentru inregistrare. </br> Ne vom auzi in curand! 👏</p>'
-  } else {
-    errorContainer.innerHTML =
-      '<p>Email-ul trebuie sa contina caracterul "@"</p>'
-  }
+  ValidateEmail()
 })
 
 subscribeInput.addEventListener('keydown', function (event) {
   if (event.key === 'Enter') {
     event.preventDefault()
-    const emailValue = subscribeInput.value
-    const emailIsValid = emailValue.includes('@')
-    if (emailIsValid) {
-      newsletterContainer.style.opacity = '0'
-      errorContainer.innerHTML =
-        '<p>Multumim pentru inregistrare. </br> Ne vom auzi in curand! 👏</p>'
-    } else {
-      errorContainer.innerHTML = '<p>Email-ul trebuie sa contina caracterul "@"'
-    }
+    ValidateEmail()
   }
 })
