9da43f053d4774e389953114d14afe7eed1a756b
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-01 19:03:05 +0200

    exploreaza retete

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function exploreRecipes () {
+  var recipes = [
+    '../pages/retete/ciorba-burta.html',
+    '../pages/retete/ciorba-grec.html',
+    '../pages/retete/cosulete-crema.html',
+    '../pages/retete/cupcake-zmeura.html',
+    '../pages/retete/ardei-umpluti.html',
+    '../pages/retete/briose-ciococlata.html',
+    '../pages/retete/caramele-sarate.html',
+    '../pages/retete/chiftele-dovlecei.html',
+    '../pages/retete/ciocanele-pui.html',
+    '../pages/retete/ciorba-miel.html',
+    '../pages/retete/fasole-verde.html',
+    '../pages/retete/frigarui-dovlecel.html',
+    '../pages/retete/friptura-miel.html',
+    '../pages/retete/fursecuri-nutella.html',
+    '../pages/retete/onion-rings.html',
+    '../pages/retete/pui-rosii.html',
+    '../pages/retete/pizza-ciuperci.html',
+    '../pages/retete/prajitura-lamaie.html',
+    '../pages/retete/pui-soia.html',
+    '../pages/retete/pulpa-porc.html',
+    '../pages/retete/sarmale.html',
+    '../pages/retete/shashuka.html',
+    '../pages/retete/spaghete-aglio.html',
+    '../pages/retete/spaghete-rosii.html',
+    '../pages/retete/supa-de-legume.html',
+    '../pages/retete/supa-linte.html',
+    '../pages/retete/supa-legume.html',
+    '../pages/retete/supa-gaina.html',
+    '../pages/retete/supa-minestrone.html',
+    '../pages/retete/supa-rosii.html',
+    '../pages/retete/tarta-lamaie.html',
+    '../pages/retete/waffe-vanilie.html'
+  ]
+
+  var randomIndex = Math.floor(Math.random() * recipes.length)
+  var randomRecipe = recipes[randomIndex]
+  window.location.href = randomRecipe
+}
--- js/search.js
+++ js/search.js
@@ -1,19 +1,41 @@
-const search = document.getElementById('search');
-const cardsContainer = document.querySelector('.retete-cards .row');
+const search = document.getElementById('search')
+const cardsContainer = document.querySelector('.retete-cards .row')
 
 search.addEventListener('keyup', e => {
-  let currentValue = e.target.value.toLowerCase();
-  let cards = document.querySelectorAll('.card');
+  let currentValue = e.target.value.toLowerCase()
+  let cards = document.querySelectorAll('.card')
 
   cards.forEach(card => {
-    let cardTitle = card.querySelector('h5');
+    let cardTitle = card.querySelector('h5')
     if (cardTitle.textContent.toLowerCase().includes(currentValue)) {
-      card.style.display = 'block';
-      
-      cardsContainer.insertBefore(card, cardsContainer.firstChild);
-    } else {
-      card.style.display = 'none';
-    }
-  });
-});
+      card.style.display = 'block'
 
+      cardsContainer.insertBefore(card, cardsContainer.firstChild)
+    } else {
+      card.style.display = 'none'
+    }
+  })
+})
+
+const favoriteButtons = document.querySelectorAll('#favorite-btn')
+
+favoriteButtons.forEach(button => {
+  button.addEventListener('click', e => {
+    e.preventDefault()
+
+    const currentCard = e.target.closest('.card')
+    const clonedCard = currentCard.cloneNode(true)
+
+    const favoritePage = window.open('pages/favorite.html', '_blank')
+    favoritePage.onload = () => {
+      const favoriteContainer =
+        favoritePage.document.querySelector('.favorite-cards')
+      favoriteContainer.appendChild(clonedCard)
+
+      const favoriteCards =
+        JSON.parse(localStorage.getItem('favoriteCards')) || []
+      favoriteCards.push(clonedCard.innerHTML)
+      localStorage.setItem('favoriteCards', JSON.stringify(favoriteCards))
+    }
+  })
+})
