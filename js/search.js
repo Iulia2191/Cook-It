de367612021074f8ecde3f55bf55477e99500299
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-04 12:42:20 +0200

    completare search

--- js/search.js
+++ js/search.js
@@ -5,28 +5,9 @@ const searchButton = document.querySelector('#search-button')
 const favoriteButtons = document.querySelectorAll('#favorite-btn')
 
 // Search input
-const urlParams = new URLSearchParams(window.location.search)
-const searchQuery = urlParams.get('query')
-if (searchQuery) {
-  search.value = searchQuery
-}
 
-search.addEventListener('keyup', function (e) {
-  const currentValue = e.target.value.toLowerCase()
-
-  cards.forEach(function (card) {
-    const cardTitle = card.querySelector('h5')
-    const titleText = cardTitle.textContent.toLowerCase()
-
-    if (titleText.includes(currentValue)) {
-      card.style.display = 'block'
-      cardsContainer.insertBefore(card, cardsContainer.firstChild)
-    } else {
-      card.style.display = 'none'
-    }
-  })
-})
-searchButton.addEventListener('click', function (e) {
+// Funcție pentru căutare
+function performSearch () {
   const currentValue = search.value.toLowerCase()
 
   cards.forEach(function (card) {
@@ -40,6 +21,39 @@ searchButton.addEventListener('click', function (e) {
       card.style.display = 'none'
     }
   })
+}
+
+// Căutare inițială după parametru
+const urlParams = new URLSearchParams(window.location.search)
+const searchQuery = urlParams.get('query')
+if (searchQuery) {
+  search.value = searchQuery
+  performSearch()
+}
+
+// Căutare la apăsarea butonului
+searchButton.addEventListener('click', function (e) {
+  performSearch()
+})
+// Cautare la enter
+search.addEventListener('keyup', function (e) {
+  performSearch()
+})
+
+// Eveniment pentru ștergerea conținutului din câmpul de căutare
+search.addEventListener('input', function (e) {
+  if (search.value === '') {
+    // Afiseaza toate cardurile
+    cards.forEach(function (card) {
+      card.style.display = 'block'
+      cardsContainer.appendChild(card)
+    })
+
+    // Elimina parametrul de cautare din URL
+    const url = new URL(window.location.href)
+    url.searchParams.delete('query')
+    window.history.replaceState({}, '', url)
+  }
 })
 
 // Adauga la favorite
