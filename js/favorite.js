a36ab9dd7d3675a53a2a91bd52e5510189c2f480
Iulia2191 <125976840+Iulia2191@users.noreply.github.com>
2023-07-11 06:54:43 +0200

    favorite

--- js/favorite.js
+++ js/favorite.js
@@ -1,45 +1,92 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const favoriteContainer = document.getElementById('favorite-container')
-  const favoriteCards = JSON.parse(localStorage.getItem('favoriteCards'))
+const favoriteButtons = document.querySelectorAll('#favorite-btn')
+const favoriteContainer = document.getElementById('favorite-container')
 
-  if (favoriteCards && favoriteCards.length > 0) {
-    favoriteCards.forEach((cardHTML, index) => {
-      console.log('Hello')
-      const cardWrapper = document.createElement('div')
-      cardWrapper.className = 'card-border'
+// Adaugă la favorite
 
-      const card = document.createElement('div')
-      card.className = 'col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3'
-      card.innerHTML = `
-        <div class="card" style="width: 18rem">
-          ${cardHTML}
-          <button class="remove-btn" data-index="${index}">Sterge</button>
-        </div>
-      `
-      cardWrapper.appendChild(card)
-      favoriteContainer.appendChild(cardWrapper)
-    })
+favoriteButtons.forEach(function (button) {
+  button.addEventListener('click', function (event) {
+    let card = event.target.closest('.card')
+    let cardData = {
+      title: card.querySelector('.card-title').textContent,
+      imageUrl: card.querySelector('.card-img-top').getAttribute('src'),
+      recipeUrl: card.querySelector('.btn').getAttribute('href')
+    }
 
-    // Butoanele de stergere
+    var favoriteRecipes = localStorage.getItem('favoriteRecipes')
+    if (favoriteRecipes) {
+      favoriteRecipes = JSON.parse(favoriteRecipes)
+      favoriteRecipes.push(cardData)
+      localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+    } else {
+      favoriteRecipes = [cardData]
+      localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+    }
+    updateFavoriteRecipes()
+  })
+})
 
-    const removeButtons = document.querySelectorAll('.remove-btn')
-    removeButtons.forEach(button => {
-      button.addEventListener('click', e => {
-        const index = e.target.dataset.index
-        removeCardFromFavorites(index)
-        e.target.parentElement.remove()
-      })
+function updateFavoriteRecipes () {
+  const favoriteRecipes =
+    JSON.parse(localStorage.getItem('favoriteRecipes')) || []
+
+  favoriteContainer.innerHTML = ''
+  if (favoriteRecipes.length > 0) {
+    favoriteRecipes.forEach((recipe, index) => {
+      const card = createRecipeCard(recipe, index)
+      favoriteContainer.appendChild(card)
     })
   } else {
-    const noFavoritesMessage = document.createElement('h3')
-    noFavoritesMessage.className = 'text-white'
-    noFavoritesMessage.textContent = 'Nu ai retete favorite.'
-    favoriteContainer.appendChild(noFavoritesMessage)
+    const emptyMessage = document.createElement('h4')
+    emptyMessage.classList.add('text-white')
+    emptyMessage.textContent = 'Nu ai retete favorite'
+    favoriteContainer.appendChild(emptyMessage)
   }
+}
 
-  function removeCardFromFavorites (index) {
-    const favoriteCards = JSON.parse(localStorage.getItem('favoriteCards'))
-    favoriteCards.splice(index, 1)
-    localStorage.setItem('favoriteCards', JSON.stringify(favoriteCards))
+function createRecipeCard (recipe, index) {
+  const card = document.createElement('div')
+  card.classList.add('card')
+
+  // Imagine
+  const image = document.createElement('img')
+  image.classList.add('card-img-top')
+  image.setAttribute('src', recipe.imageUrl)
+  card.appendChild(image)
+
+  // Titlul
+  const title = document.createElement('h2')
+  title.classList.add('card-title')
+  title.textContent = recipe.title
+  card.appendChild(title)
+
+  // Buton Vezi reteta
+  const link = document.createElement('a')
+  link.classList.add('btn')
+  link.setAttribute('href', recipe.recipeUrl)
+  link.textContent = 'Vezi reteta'
+  card.appendChild(link)
+
+  // Buton Sterge
+  const removeButton = document.createElement('button')
+  removeButton.classList.add('remove-btn')
+  removeButton.textContent = 'Sterge'
+  removeButton.addEventListener('click', function () {
+    removeRecipeFromFavorites(index)
+  })
+  card.appendChild(removeButton)
+  return card
+}
+
+function removeRecipeFromFavorites (index) {
+  const favoriteRecipes =
+    JSON.parse(localStorage.getItem('favoriteRecipes')) || []
+
+  if (index >= 0 && index < favoriteRecipes.length) {
+    favoriteRecipes.splice(index, 1)
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipes))
+
+    updateFavoriteRecipes()
   }
-})
+}
+
+updateFavoriteRecipes()
--- js/search.js
+++ js/search.js
@@ -2,7 +2,6 @@ const search = document.getElementById('search')
 const cardsContainer = document.querySelector('.retete-cards .row')
 const cards = document.querySelectorAll('.card')
 const searchButton = document.querySelector('#search-button')
-const favoriteButtons = document.querySelectorAll('#favorite-btn')
 
 // Search input
 
@@ -47,26 +46,4 @@ search.addEventListener('input', function (e) {
     url.searchParams.delete('query')
     window.history.replaceState({}, '', url)
   }
-})
-
-// Adauga la favorite
-favoriteButtons.forEach(button => {
-  button.addEventListener('click', e => {
-    e.preventDefault()
-
-    const currentCard = e.target.closest('.card')
-    const clonedCard = currentCard.cloneNode(true)
-
-    const favoritePage = window.open('pages/favorite.html', '_blank')
-    favoritePage.onload = () => {
-      const favoriteContainer =
-        favoritePage.document.querySelector('.favorite-cards')
-      favoriteContainer.appendChild(clonedCard)
-
-      const favoriteCards =
-        JSON.parse(localStorage.getItem('favoriteCards')) || []
-      favoriteCards.push(clonedCard.innerHTML)
-      localStorage.setItem('favoriteCards', JSON.stringify(favoriteCards))
-    }
-  })
 })
