4812f15ad544666e9206b7e2a02272d6b19d37d2
Iulia2191 <ibocaniciu@gmail.com>
2023-07-21 14:59:35 +0300

    corecatre contact

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ const searchForm = document.getElementById('search-form')
 const searchInput = document.getElementById('search-input')
 
 // Email validation Subscribe Input
-function validateEmail () {
+function validateSubscribeEmail () {
   var validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
   if (subscribeInput.value.match(validRegex)) {
@@ -26,13 +26,13 @@ function validateEmail () {
 
 function handleClick (event) {
   event.preventDefault()
-  validateEmail()
+  validateSubscribeEmail()
 }
 
 function handleEnter (event) {
   if (event.key === 'Enter') {
     event.preventDefault()
-    validateEmail()
+    validateSubscribeEmail()
   }
 }
 
