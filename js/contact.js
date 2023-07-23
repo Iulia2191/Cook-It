6fab05d0697ad52a1475d3a5a90809e61f781e05
Iulia2191 <ibocaniciu@gmail.com>
2023-07-23 18:45:32 +0300

    comm titlu

--- js/contact.js
+++ js/contact.js
@@ -84,11 +84,7 @@ submitButton.addEventListener('click', function (event) {
   }
 })
 
-<<<<<<< Updated upstream
 // Textarea numar caractere
-
-=======
->>>>>>> Stashed changes
 const characterCount = document.getElementById('characterCount')
 const totalChars = 20
 
